4bdd9b2dd021aa5bc79df9df3531a492a2edf9d4
Can Ural <can.ural@outlook.com>
2019-11-30 12:30:18 +0300

    input component added and styled

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./form-input.styles.scss";
+
+const FormInput = ({ handleChange, label, ...otherProps}) => (
+    <div className="group">
+        <input className="form-input" onChange={handleChange} {...otherProps}/>{ /* we call on change as handleChange in here */}
+        {    
+            label ?  /* if we want to create a label it creates a label, if not null(doesnt create) */
+            (<label
+                className={`${
+                    otherProps.value.length ? "shrink" : ""  /* if a user typed in something, then apply shrink value, if not, nothing */
+                } form-input-label`} /* In any case, apply form input label */
+            >
+            {label}
+            </label>)
+            : null
+        }
+    </div>
+
+)
+
+export default FormInput;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,50 +1,59 @@
-import React from "react";
+import React from 'react';
+import FormInput from '../form-input/form-input.component';
 
-import "./sign-in.styles.scss";
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
+	constructor(props) {
+		super(props);
 
-    constructor(props) {
-        super(props);
-        
+		this.state = {
+			email: '',
+			password: ''
+		};
+	}
 
-        this.state = {
-            email: "",
-            password: ""
-        }
-    }
+	handleSubmit = (e) => {
+		e.preventDefault();
 
-    handleSubmit = e => {
-        e.preventDefault();
+		this.setState({ email: '', password: '' });
+	};
 
-        this.setState({ email:"", password:""})
-    }
-    
-    handleChange = e => {
-        const { value, name } = e.target;
+	handleChange = (e) => {
+		const { value, name } = e.target;
 
-        this.setState({ [name]: value })
-    }
+		this.setState({ [name]: value });
+	};
 
+	render() {
+		return (
+			<div className="sign-in">
+				<h2>I already have an account</h2>
+				<span>Sign in with your email and password</span>
 
-    render () {
-        return (
-            <div className="sign-in">
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-
-
-                <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" value={this.state.email} onChange={this.handleChange} required/>
-                    <label >Email</label>
-                    <input name="password" type="password" value={this.state.password} onChange={this.handleChange} required/>
-                    <label >Password</label>
-
-                    <input type="submit" value="Submit Form"/>
-                </form>
-            </div>
-        )
-    }
+				<form onSubmit={this.handleSubmit}>
+					<FormInput
+						name="email"
+						type="email"
+						value={this.state.email}
+						handleChange={this.handleChange}
+						label="email"
+						required
+					/>
+					{/*we changed onChange to handleChange here because of form input component, thats how we call it there */}
+					<FormInput
+						name="password"
+						type="password"
+						value={this.state.password}
+						handleChange={this.handleChange}
+						label="password"
+						required
+					/>
+					<input type="submit" value="Submit Form" />
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SignIn;
