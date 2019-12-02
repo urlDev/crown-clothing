f2a147c889ca533641dd29396d9a615eac4b9368
Can Ural <can.ural@outlook.com>
2019-12-02 18:32:24 +0300

    redux added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// Provider comes from react
+import { Provider } from "react-redux";
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	// Provider must be parent of all components in order to work
+	<Provider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
+	
 	document.getElementById('root')
 );
